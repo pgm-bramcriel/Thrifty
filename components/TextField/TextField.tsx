4a22aa0cc9b743578fe110b52b9ed00d00469970
pgm-bramcriel <bramcrie@student.arteveldehs.be>
2022-02-22 11:47:53 +0100

    fixed styling

--- components/TextField/TextField.tsx
+++ components/TextField/TextField.tsx
@@ -1,17 +1,18 @@
 import React, { ReactPropTypes } from 'react'
 import { ErrorMessage, useField } from 'formik'
+import { TextFieldStyle, TextFieldContainer } from './style';
 
 const TextField = ({label, ...props}: any) => {
   const [field, meta] = useField(props);
   return (
-    <div className="mb-2">
+    <TextFieldContainer className="mb-2">
       <label htmlFor={field.name}>{label}</label>
-      <input
-        className={`form-control shadow-none ${meta.touched && meta.error && 'is-invalid'}`}
+      <TextFieldStyle
+        className={`form-control ${meta.touched && meta.error && 'is-invalid'}`}
         {...field} {...props}
       />
       <ErrorMessage name={field.name}/>
-    </div>
+    </TextFieldContainer>
   )
 }
 
