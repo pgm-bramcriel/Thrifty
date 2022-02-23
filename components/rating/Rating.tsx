55cd0a25caaf36a0de9b71ab5708d69448a2f923
pgm-bramcriel <bramcrie@student.arteveldehs.be>
2022-02-23 16:36:34 +0100

    updated rating functionality

--- components/rating/Rating.tsx
+++ components/rating/Rating.tsx
@@ -6,14 +6,36 @@ import { RatingStyling, Upvote, Downvote } from './style'
 
 const Rating = (props: any) => {
   const [ rating, setRating ] = useState(props.rating);
+  const [ isUpvoted, setIsUpvoted ] = useState(false);
+  const [ isDownvoted, setIsDownvoted ] = useState(false);
+
+  const handleUpvote = () => {
+    if (!isUpvoted) {
+      setRating(rating + 1);
+      setIsUpvoted(true);
+      setIsDownvoted(false);
+    } else {
+      setRating(rating);
+    }
+  }
+
+  const handleDownvote = () => {
+    if (!isDownvoted) {
+      setRating(rating - 1);
+      setIsDownvoted(true);
+      setIsUpvoted(false);
+    } else {
+      setRating(rating);
+    }
+  }
 
   return (
     <RatingStyling>
-      <Upvote onClick={() => setRating(rating + 1)}>
+      <Upvote onClick={handleUpvote}>
         <KeyboardArrowUpIcon/>
       </Upvote>
       <span>{rating}</span>
-      <Downvote onClick={() => rating > 0 ? setRating(rating - 1) : ''}>
+      <Downvote onClick={handleDownvote}>
         <KeyboardArrowDownIcon />
       </Downvote>
     </RatingStyling>
