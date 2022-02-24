b3efd5ea22e57691bb4d28d86afc7d4ce3569817
Branko Devroe <branko.devroe@student.odisee.be>
2022-02-24 10:22:39 +0100

    Voting system is working

--- components/rating/Rating.tsx
+++ components/rating/Rating.tsx
@@ -8,70 +8,96 @@ const Rating = (props: any) => {
   const [rating, setRating] = useState(props.rating);
   const [isUpvoted, setIsUpvoted] = useState(false);
   const [isDownvoted, setIsDownvoted] = useState(false);
+  const [ratingMin, setRatingMin] = useState(props.rating - 1);
+  const [ratingMax, setRatingMax] = useState(props.rating + 1);
 
-  const handleUpvote = () => {
+  const handleUpvote = async () => {
+    if(rating !== ratingMax) {
+      setRating(rating + 1);
+
+      console.log(ratingMax);
+
+      await fetch('http://localhost:3000/api/updateRating', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'itemId': props.itemId,
+          'vote_count': rating + 1,
+        })
+      })
+    }
+  }
+
+  const handleDownvote = async () => {
+    if(rating !== ratingMin) {
+      setRating(rating - 1);
+
+      console.log(ratingMax);
+
+      await fetch('http://localhost:3000/api/updateRating', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          'itemId': props.itemId,
+          'vote_count': rating - 1,
+        })
+      })
+    }
+  }
+
+  /*
+  const handleUpvote = async () => {
     if (!isUpvoted) {
       setRating(rating + 1);
       setIsUpvoted(true);
       setIsDownvoted(false);
 
-      fetch('http://localhost:3000/api/updateRating', {
+      console.log(rating);
+
+      await fetch('http://localhost:3000/api/updateRating', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           'itemId': props.itemId,
-          'vote_count': rating,
+          'vote_count': rating + 1,
         })
       })
     } else {
       setRating(rating);
-
-      fetch('http://localhost:3000/api/updateRating', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          'itemId': props.itemId,
-          'vote_count': rating,
-        })
-      })
     }
   }
+  */
 
-  const handleDownvote = () => {
+  /*
+  const handleDownvote = async () => {
     if (!isDownvoted) {
       setRating(rating - 1);
       setIsDownvoted(true);
       setIsUpvoted(false);
 
-      fetch('http://localhost:3000/api/updateRating', {
+      console.log(rating);
+
+      await fetch('http://localhost:3000/api/updateRating', {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
           'itemId': props.id,
-          'vote_count': rating,
+          'vote_count': rating - 1,
         })
       })
     } else {
       setRating(rating);
-
-      fetch('http://localhost:3000/api/updateRating', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          'itemId': props.id,
-          'vote_count': rating,
-        })
-      })
     }
   }
+  */
 
   return (
     <RatingStyling>
